128cc14f9c30b32cf4eb0edd2b625a54a9fc37bb
Vitali Sinitski <vitali.sin777@gmail.com>
2023-01-12 02:51:33 +0300

    Changed task from homework04

--- src/main/java/vitali/homework/lesson04/person/Person.java
+++ src/main/java/vitali/homework/lesson04/person/Person.java
@@ -31,6 +31,18 @@ public class Person implements Serializable {
         return age;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name) && Objects.equals(surname, person.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age);
+    }
 
     @Override
     public String toString() {
